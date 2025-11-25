e8f5931585172bf6bb8a171d31d7c5478096ad80
akshayabhat12 <akshaya.bhat12@gmail.com>
2025-11-25 23:42:51 +0530

    Time: 0 ms (100%), Space: 28.3 MB (24.1%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
-        int even = 0, odd = 0;
+        int even=0,odd=0;
         vector<int> ans;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] % 2 == 0)
-                even++;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]%2==0)
+                ans.insert(ans.begin(),0);
             else
-                odd++;
-        }
-
-        // Insert 'even' number of 0s
-        for (int i = 0; i < even; i++) {
-            ans.push_back(0);
-        }
-
-        // Insert 'odd' number of 1s
-        for (int i = 0; i < odd; i++) {
-            ans.push_back(1);
+                ans.push_back(1);
         }
 
         return ans;
+
     }
 };
